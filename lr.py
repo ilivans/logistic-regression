26737a1b11b2dbdf1d70b3ccc2f53eae1f091eb5
Ilya Ivanov <ilya.ivanov.se@gmail.com>
2017-03-12 15:49:01 +0300

    logreg

---
+++ lr.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy import special
+from sklearn.base import ClassifierMixin, BaseEstimator
+from sklearn.datasets import make_classification
+
+# Используйте scipy.special для вычисления численно неустойчивых функций
+# https://docs.scipy.org/doc/scipy/reference/special.html#module-scipy.special
+
+
+def lossf(w, X, y, l1, l2):
+    """
+    Вычисление функции потерь.
+
+    :param w: numpy.array размера  (M,) dtype = np.float
+    :param X: numpy.array размера  (N, M), dtype = np.float
+    :param y: numpy.array размера  (N,), dtype = np.int
+    :param l1: float, l1 коэффициент регуляризатора 
+    :param l2: float, l2 коэффициент регуляризатора 
+    :return: float, value of loss function
+    """
+    lossf = special.log1p(special.expm1((-y.reshape((-1, 1)) * X).dot(w)) + 1.).sum() + l1 * np.abs(w).sum() + l2 * (w**2).sum()
+    return lossf
+
+
+def gradf(w, X, y, l1, l2):
+    """
+    Вычисление градиента функции потерь.
+
+    :param w: numpy.array размера  (M,), dtype = np.float
+    :param X: numpy.array размера  (N, M), dtype = np.float
+    :param y: numpy.array размера  (N,), dtype = np.int
+    :param l1: float, l1 коэффициент регуляризатора 
+    :param l2: float, l2 коэффициент регуляризатора 
+    :return: numpy.array размера  (M,), dtype = np.float, gradient vector d lossf / dw
+    """
+    yx = -y.reshape((-1, 1)) * X
+    gradw = (special.expit(yx.dot(w)).reshape((-1, 1)) * yx).sum(axis=0) + l1 * np.sign(w).sum() + l2 * 2 * w.sum()
+    return gradw
+
+
+class LR(ClassifierMixin, BaseEstimator):
+    def __init__(self, lr=1, l1=1e-4, l2=1e-4, num_iter=1000, verbose=0):
+        self.l1 = l1
+        self.l2 = l2
+        self.w = None
+        self.lr = lr
+        self.verbose = verbose
+        self.num_iter = num_iter
+
+    def fit(self, X, y):
+        """
+        Обучение логистической регрессии.
+        Настраивает self.w коэффициенты модели.
+
+        Если self.verbose == True, то выводите значение 
+        функции потерь на итерациях метода оптимизации. 
+
+        :param X: numpy.array размера  (N, M), dtype = np.float
+        :param y: numpy.array размера  (N,), dtype = np.int
+        :return: self
+        """
+        n, d = X.shape
+        self.w = np.random.random(X.shape[1]) / 2.
+
+        for _ in xrange(self.num_iter):
+            if self.verbose:
+                print lossf(self.w, X, y, self.l1, self.l2)
+            grad = gradf(self.w, X, y, self.l1, self.l2)
+            self.w -= self.lr * grad
+        return self
+
+    def predict_proba(self, X):
+        """
+        Предсказание вероятности принадлежности объекта к классу 1.
+        Возвращает np.array размера (N,) чисел в отрезке от 0 до 1.
+
+        :param X: numpy.array размера  (N, M), dtype = np.float
+        :return: numpy.array размера  (N,), dtype = np.int
+        """
+        # Вычислите вероятности принадлежности каждого 
+        # объекта из X к положительному классу, используйте
+        # эту функцию для реализации LR.predict
+        probs = special.expit(X.dot(self.w))
+        return probs
+
+    def predict(self, X):
+        """
+        Предсказание класса для объекта.
+        Возвращает np.array размера (N,) элементов 1 или -1.
+
+        :param X: numpy.array размера  (N, M), dtype = np.float
+        :return:  numpy.array размера  (N,), dtype = np.int
+        """
+        # Вычислите предсказания для каждого объекта из X
+        predicts = np.where(self.predict_proba(X) > 0.5, 1, -1)
+        return predicts 
+
+
+def test_work():
+    print "Start test"
+    X, y = make_classification(n_features=100, n_samples=1000)
+    y = 2 * (y - 0.5)
+
+    try:
+        clf = LR(lr=1, l1=1e-4, l2=1e-4, num_iter=1000, verbose=1)
+    except Exception:
+        assert False, "Создание модели завершается с ошибкой"
+        return
+
+    # clf = clf.fit(X, y)
+
+    try:
+        clf = clf.fit(X, y)
+    except Exception:
+        assert False, "Обучение модели завершается с ошибкой"
+        return
+
+    assert isinstance(lossf(clf.w, X, y, 1e-3, 1e-3), float), "Функция потерь должна быть скалярной и иметь тип np.float"
+    assert gradf(clf.w, X, y, 1e-3, 1e-3).shape == (100,), "Размерность градиента должна совпадать с числом параметров"
+    assert gradf(clf.w, X, y, 1e-3, 1e-3).dtype == np.float, "Вектор градиента, должен состоять из элементов типа np.float"
+    assert clf.predict(X).shape == (1000,), "Размер вектора предсказаний, должен совпадать с количеством объектов"
+    assert np.min(clf.predict_proba(X)) >= 0, "Вероятности должны быть не меньше, чем 0"
+    assert np.max(clf.predict_proba(X)) <= 1, "Вероятности должны быть не больше, чем 1"
+    assert len(set(clf.predict(X))) == 2, "Метод предсказывает больше чем 2 класса на двух классовой задаче"
+    print "End tests"
+
+test_work()
